603119a71b0200aacb5fab6be329d4ad89efc91b
Edgar Hernandez <edgar.lt@hotmail.com>
2024-11-12 23:11:06 -0600

    Archivos actualizados para finalizar la actividad/proyecto

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres1@localhost:5433/mi_flask_app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Aquí puedes añadir más configuraciones
